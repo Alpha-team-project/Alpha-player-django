2f395a9843ea82debbe9784f5100ffce0e9b03cf
bahrom04 <magdiyevbahrom@gmail.com>
2025-03-19 11:54:54 +0500

    adding dotenv package and modifiactions

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ SECRET_KEY = 'django-insecure-_qqcv6!huk2hj!e$&meiq5nh@)a%#ev5e!=zsg1u_=y+sl=88l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
  
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
