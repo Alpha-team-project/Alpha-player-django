c667347b3ad63cb8d521056541ec17e5681ea2e2
bahrom04 <magdiyevbahrom@gmail.com>
2025-04-09 13:00:35 +0500

    add: item test model

--- item/tests.py
+++ item/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from item.models import Category, Author, Music
 
-# Create your tests here.
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name="Pop")
+
+    def test_category_name_created(self):
+        """Test name equal or not"""
+        category = Category.objects.get(name="Pop")
+        self.assertEqual(category.name, 'Pop')
+
+class AuthourTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='johndoe', password='testpassword')
+
+        Author.objects.create(
+            user=self.user, full_name="John Doe",
+            image="./static/images/browse_page.png",
+            description="test data")
+
+    def test_author_model_created(self):
+        author = Author.objects.get(user=self.user)
+
+        self.assertEqual(author.full_name, "John Doe")
+        self.assertEqual(author.image, "./static/images/browse_page.png")
+        self.assertEqual(author.description, "test data")
+       
