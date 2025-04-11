621c53bcf86f645cd74eb20939c2b7579e1508b4
bahrom04 <magdiyevbahrom@gmail.com>
2025-04-11 11:46:32 +0500

    clear: git cache

--- dashboard/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms 
-from django.contrib.auth.models import User
-from item.models import Category
-
-class CategoryForm(forms.ModelForm):
-    class Meta:
-        model = Category
-        fields = ('name',)
-        
--- item/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Category, Music, Author
-
-admin.site.register(Category)
-admin.site.register(Music)
-admin.site.register(Author)
