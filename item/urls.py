9a154011ed86cd10cc958ce3ef1b00d9f709118f
bahrom04 <magdiyevbahrom@gmail.com>
2025-04-15 09:36:29 +0500

    fix: redirect music

--- item/urls.py
+++ item/urls.py
@@ -5,7 +5,7 @@ app_name = 'item'
 
 urlpatterns = [
     path('',views.music_list, name='items'),
-    path('<int:pk>/', views.music_detail, name = 'detail'),
+    path('<int:pk>/', views.music_detail, name = 'music_detail'),
     path('new/',views.music_new, name='new'),
     path('<int:pk>/delete', views.music_delete, name = 'delete'),
     path('<int:pk>/edit', views.music_edit, name = 'edit'),
