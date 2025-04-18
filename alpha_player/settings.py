9a4e57b4af1f1f1014eb39a108e36aa43022eec6
bahrom04 <magdiyevbahrom@gmail.com>
2025-04-18 15:57:27 +0500

    debug: ci pipeline

--- alpha_player/settings.py
+++ alpha_player/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = "alpha_player.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", "5432"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
