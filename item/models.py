27437378d2a4c54b8ef0e4b9a30d08c363295ac9
Tolqinov Otabek <otabektolqinov30@gmail.com>
2025-04-21 09:32:01 +0500

    Comments added

--- item/models.py
+++ item/models.py
@@ -2,18 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+# todo: all the models are saved here
+
+# todo: model for Categories
 class Category(models.Model):
+
+
     name = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # it ensures are ordered in a natural order by their names
     class Meta:
         ordering = ('name',)
         verbose_name_plural = 'Categories'
 
+    # todo: this provides the chance when we call the instance of category, we will get details of instance
     def __str__(self):
         return self.name
 
+# todo: model for Categories
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='author_profile')
     full_name = models.CharField(max_length=255)
@@ -22,9 +30,11 @@ class Author(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # todo: this provides the chance when we call the instance of category, we will get details of instance
     def __str__(self):
         return self.full_name
 
+# todo: model for Musics
 class Music(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='musics')
@@ -39,8 +49,10 @@ class Music(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
 
+    # todo: this provides the chance when we call the instance of category, we will get details of instance
     def __str__(self):
         return self.title
 
+    # todo: this orders musics by created_at
     class Meta:
         ordering = ('-created_at',)
